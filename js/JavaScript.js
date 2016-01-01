22ab21486de84ab84449143b82fc26a1ce5a8c30
DruzhyninB <druzhyninbogdan@gmail.com>
2016-01-01 04:11:47 +0200

    first commit

---
+++ js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿function go() {
+
+}
+
+$(window).ready(function () {
+    $('.preloader').animate({
+        opacity:0
+    }, 1000, function () {
+        $('.preloader').remove();
+        $('#audio').prop('volume',0.08);
+        $('#audio').trigger('play');
+        go();
+    })
+})
